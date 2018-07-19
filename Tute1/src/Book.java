2b43d8a80b81afbbe1c0b80c8a5764f3f25314fc
Elliot Schot <s3530160@student.rmit.edu.au>
2018-07-19 17:49:21 +1000

    adding part d

--- Tute1/src/Book.java
+++ Tute1/src/Book.java
@@ -1,20 +1,37 @@
 public class Book {
 
-// implement requirements A - D here
-	private String	bookID;
-	private String	title;
-	private String	author;
-	private String	borrowerID;
-// ...
+	// implement requirements A - D here
+	private String bookID;
+	private String title;
+	private String author;
+	private String borrowerID;
 
-	public Book(String bookID, String title, String author)
-	{
+	public Book(String bookID, String title, String author) {
 		super();
 		this.bookID = bookID;
 		this.title = title;
 		this.author = author;
 	}
-	
+
+	public String getBookID() {
+		return bookID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	 public boolean borrow(String borrowerID) {
+		 if (this.borrowerID == null) {
+			 this.borrowerID = borrowerID;
+			 return true;
+		 } else {
+			 return false;
+		 }
+	 }
+	 
+// ...
+
 // method which allows the Book to be "returned" if it has been borrowed 
 	public boolean returnBook() {
 		// if borrower ID has been set (ie. book has been borrowed)

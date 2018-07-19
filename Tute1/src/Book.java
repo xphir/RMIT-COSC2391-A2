92778c249bb2321545bd0c656c92137eda490392
Elliot Schot <s3530160@student.rmit.edu.au>
2018-07-19 17:36:29 +1000

    added book constructor

--- Tute1/src/Book.java
+++ Tute1/src/Book.java
@@ -1,38 +1,40 @@
-public class Book
-{
+public class Book {
 
 // implement requirements A - D here
-
+	private String	bookID;
+	private String	title;
+	private String	author;
+	private String	borrowerID;
 // ...
 
-
-
+	public Book(String bookID, String title, String author)
+	{
+		super();
+		this.bookID = bookID;
+		this.title = title;
+		this.author = author;
+	}
+	
 // method which allows the Book to be "returned" if it has been borrowed 
- public boolean returnBook()
- {
-  // if borrower ID has been set (ie. book has been borrowed)
-  if (this.borrowerID != null)
-  {
-  
+	public boolean returnBook() {
+		// if borrower ID has been set (ie. book has been borrowed)
+		if (this.borrowerID != null) {
+
 // reset borrowerID to null and return true result 
-   this.borrowerID = null;
-   return true;
-  }
-else
-  {
-  
+			this.borrowerID = null;
+			return true;
+		} else {
+
 // otherwise book is not currently borrowed so return false result
-   return false;
-  }
- }
- 
+			return false;
+		}
+	}
+
 // format Book details to a String (useful for printing!)
-public String toString() 
-{
- return String.format(
-  
-"Book ID: %s\nTitle: %s\nAuthor: %s\nBorrowed by: %s\n",
-   this.bookID, this.title, this.author,
-   this.borrowerID != null ? this.borrowerID : "Available"); 
- }
-} 
+	public String toString() {
+		return String.format(
+
+				"Book ID: %s\nTitle: %s\nAuthor: %s\nBorrowed by: %s\n", this.bookID, this.title, this.author,
+				this.borrowerID != null ? this.borrowerID : "Available");
+	}
+}

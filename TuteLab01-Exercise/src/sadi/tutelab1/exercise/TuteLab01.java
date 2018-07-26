78f0b42d3de601f72fa452f30e4e40238bc34a74
Unknown <s3530160@student.rmit.edu.au>
2018-07-26 16:25:34 +1000

    working update

--- TuteLab01-Exercise/src/sadi/tutelab1/exercise/TuteLab01.java
+++ TuteLab01-Exercise/src/sadi/tutelab1/exercise/TuteLab01.java
@@ -16,9 +16,6 @@ public class TuteLab01
 	static double amps[];
 	static double resistance[];
 	static Scanner scanner = new Scanner(System.in);
-	static double v;
-	static double i;
-	static double r;
 	static boolean doAgain;
 	static double vavg;
 	static double vmax;
@@ -40,74 +37,247 @@ public class TuteLab01
 
 		System.out.println("Enter 3 pairs of V and I");
 
+		doLoop();
+
+		calculateVolts();
+		calculateAmps();
+		calculateResistance();
+		
+		System.out.format("V (Volts) .. Avg=%f, Max=%f, Min=%f%n", vavg,vmax,vmin);
+		
+
+		System.out.format("I (Amperes) .. Avg=%f, Max=%f, Min=%f%n", iavg,imax,imin);
+		
+
+		System.out.format("R (Ohms) .. Avg=%f, Max=%f, Min=%f%n", ravg,rmax,rmin);
+
+		
+	}
+	
+
+
+
+	public static void calculateVolts() {	
+		setVavg(getAvg(volts));
+		setVmax(getMax(volts));
+		setVmin(getMin(volts));
+	}
+	
+	public static void calculateAmps() {
+		setIavg(getAvg(amps));
+		setImax(getMax(amps));
+		setImin(getMin(amps));
+	}
+	
+	public static void calculateResistance() {
+		setRavg(getAvg(resistance));
+		setRmax(getMax(resistance));
+		setRmin(getMin(resistance));
+	}
+	
+	public static void doLoop() {
+		
 		for (int count = 0; count < 3; count++)
 		{
-			doAgain = true;
-			double num = 0;
+			volts[count] = doSingle("Enter Voltage (V) in volts: ");
 
-			do
-			{
-				try
-				{
-					System.out.print("Enter Voltage (V) in volts: ");
-					// parseDouble() will "throw" an exception if a number is not
-					// entered. Execution then immediately moves to catch block
-					num = Double.parseDouble(scanner.nextLine());
-					doAgain = false;
-				}
-				catch (NumberFormatException nfe)
-				{
-					// we don't really need to do anything here since we will just loop and try again, but could.
-				}
-			}
-			while (doAgain == true);
+			amps[count] = doSingle("Enter Current (I) in amperes: ");
 
-			v = num;
-			volts[count] = v;
+			resistance[count] = volts[count] / amps[count];
 
-			doAgain = true;
-
-			do
-			{
-				try
-				{
-					System.out.print("Enter Current (I) in amperes: ");
-					// parseDouble() will "throw" an exception if a number is not
-					// entered. Execution then immediately moves to catch block
-					num = Double.parseDouble(scanner.nextLine());
-					doAgain = false;
-				}
-				catch (NumberFormatException nfe)
-				{
-					// we don't really need to do anything here since we will just loop and try again, but could.
-				}
-			}
-			while (doAgain == true);
-
-			i = num;
-			amps[count] = i;
-			r = v / i;
-			resistance[count] = r;
-
-			System.out.println("Resistance R is " + r + " ohms\n");
+			System.out.format("Resistance R is %f ohms\n\n",resistance[count]);
 		}
+	}
+	
+	
+	public static double doSingle(String printToScreen) {
+		doAgain = true;
+		double value;
+		do
+		{
+			value = getValue(printToScreen);
+		}
+		while (doAgain == true);
 
-		vavg = (volts[0] + volts[1] + volts[2]) / 3;
-		vmax = Math.max(Math.max(volts[0], volts[1]), volts[2]);
-		vmin = Math.min(Math.min(volts[0], volts[1]), volts[2]);
+		return value;
+	}
+	
 
-		System.out.println("V (Volts) .. Avg=" + vavg + ", Max=" + vmax + ", Min=" + vmin);
+	public static double getAvg(double inputArray[] ) {
+		double avg = 0;
+		double total = 0;
+		for (int i = 0; i < inputArray.length; i++)
+		{
+		     total = total + inputArray[i];
+		}
+		avg = total / inputArray.length;
+		return avg;
+	}
+	
+	public static double getMin(double inputArray[] ) {
+		double min = inputArray[0];
+		for (int i = 0; i < inputArray.length; i++)
+		{
+		     if (inputArray[i] < min)
+		     {
+		      min = inputArray[i];
+		     }
+		}
+		return min;
+	}
+	
+	public static double getMax(double inputArray[] ) {
+		double max = inputArray[0];
+		for (int i = 0; i < inputArray.length; i++)
+		{
+		     if (inputArray[i] > max)
+		     {
+		      max = inputArray[i];
+		     }
+		}
+		return max;
+	}
+	
+	public static double getValue(String printToScreen) {
+		double num = 0;
+		try
+		{
+			System.out.print(printToScreen);
+			// parseDouble() will "throw" an exception if a number is not
+			// entered. Execution then immediately moves to catch block
+			num = Double.parseDouble(scanner.nextLine());
+			doAgain = false;
+		}
+		catch (NumberFormatException nfe)
+		{
+			// we don't really need to do anything here since we will just loop and try again, but could.
+		}
+		return num;
+	}
 
-		iavg = (amps[0] + amps[1] + amps[2]) / 3;
-		imax = Math.max(Math.max(amps[0], amps[1]), amps[2]);
-		imin = Math.min(Math.min(amps[0], amps[1]), amps[2]);
 
-		System.out.println("I (Amperes) .. Avg=" + iavg + ", Max=" + imax + ", Min=" + imin);
 
-		ravg = (resistance[0] + resistance[1] + resistance[2]) / 3;
-		rmax = Math.max(Math.max(resistance[0], resistance[1]), resistance[2]);
-		rmin = Math.min(Math.min(resistance[0], resistance[1]), resistance[2]);
 
-		System.out.println("R (Ohms) .. Avg=" + ravg + ", Max=" + rmax + ", Min=" + rmin);
+	public static double getVavg() {
+		return vavg;
+	}
+
+
+
+
+	public static void setVavg(double vavg) {
+		TuteLab01.vavg = vavg;
+	}
+
+
+
+
+	public static double getVmax() {
+		return vmax;
+	}
+
+
+
+
+	public static void setVmax(double vmax) {
+		TuteLab01.vmax = vmax;
+	}
+
+
+
+
+	public static double getVmin() {
+		return vmin;
+	}
+
+
+
+
+	public static void setVmin(double vmin) {
+		TuteLab01.vmin = vmin;
+	}
+
+
+
+
+	public static double getIavg() {
+		return iavg;
+	}
+
+
+
+
+	public static void setIavg(double iavg) {
+		TuteLab01.iavg = iavg;
+	}
+
+
+
+
+	public static double getImax() {
+		return imax;
+	}
+
+
+
+
+	public static void setImax(double imax) {
+		TuteLab01.imax = imax;
+	}
+
+
+
+
+	public static double getImin() {
+		return imin;
+	}
+
+
+
+
+	public static void setImin(double imin) {
+		TuteLab01.imin = imin;
+	}
+
+
+
+
+	public static double getRavg() {
+		return ravg;
+	}
+
+
+
+
+	public static void setRavg(double ravg) {
+		TuteLab01.ravg = ravg;
+	}
+
+
+
+
+	public static double getRmax() {
+		return rmax;
+	}
+
+
+
+
+	public static void setRmax(double rmax) {
+		TuteLab01.rmax = rmax;
+	}
+
+
+
+
+	public static double getRmin() {
+		return rmin;
+	}
+
+
+
+
+	public static void setRmin(double rmin) {
+		TuteLab01.rmin = rmin;
 	}
 }

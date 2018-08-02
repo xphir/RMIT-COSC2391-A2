ba0d1c1ab62e6d5806c25c9a29bd0de3704ebab0
Unknown <s3530160@student.rmit.edu.au>
2018-08-02 17:32:51 +1000

    tute02 progress

--- Tutes/Tute02/src/cms/model/vehicle/Truck.java
+++ Tutes/Tute02/src/cms/model/vehicle/Truck.java
@@ -1,5 +1,16 @@
 package cms.model.vehicle;
 
-public class Truck {
+import cms.model.Vechicle;
 
+public class Truck extends Vechicle{
+	private double loadCapacity;
+
+	public Truck(String registrationNumber, String make, String model, int year, double odometer,
+			double serviceInterval, double loadCapacity) {
+		super(registrationNumber, make, model, year, odometer, serviceInterval);
+		loadCapacity = loadCapacity;
+		// TODO Auto-generated constructor stub
+	}
+
+	
 }
--- Tutes/Tute02/src/cms/model/vehicle/Van.java
+++ Tutes/Tute02/src/cms/model/vehicle/Van.java
@@ -1,5 +1,11 @@
 package cms.model.vehicle;
 
-public class Van {
+import cms.model.Vechicle;
+
+public class Van extends Vechicle {
+	public Van(String registrationNumber, String make, String model, int year, double odometer, double serviceInterval) {
+		super(registrationNumber, make, model, year, odometer, serviceInterval);
+		// TODO Auto-generated constructor stub
+	}
 
 }

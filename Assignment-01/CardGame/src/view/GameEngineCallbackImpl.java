8f248542c0a425158c7bb5462bb29a7b74e77fd0
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-24 22:44:22 +1000

    fix logging

--- Assignment-01/CardGame/src/view/GameEngineCallbackImpl.java
+++ Assignment-01/CardGame/src/view/GameEngineCallbackImpl.java
@@ -60,48 +60,42 @@ public class GameEngineCallbackImpl implements GameEngineCallback
    public void nextCard(Player player, PlayingCard card, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, "Intermediate data to log .. String.format() is good here!");
-      // TODO: complete this method to log results
+      logger.log(Level.FINE, String.format("Card Dealt to %s .. Suit: %s, Value: %s, Score: %d",player.getPlayerName(),card.getSuit(),card.getValue(),card.getScore()));
    }
 
    @Override
    public void result(Player player, int result, GameEngine engine)
    {
       // final results logged at Level.INFO
-      logger.log(Level.INFO, "Result data to log .. String.format() is good here!");
-      // TODO: complete this method to log results
+      logger.log(Level.INFO, String.format("The Loser, final result=%d", result));
    }
 
    @Override
    public void bustCard(Player player, PlayingCard card, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, "Intermediate data to log .. String.format() is good here!");
-      // TODO: complete this method to log results
+      logger.log(Level.FINE, String.format("Card Dealt to %s .. Suit: %s, Value: %s, Score: %d ... YOU BUSTED!",player.getPlayerName(),card.getSuit(),card.getValue(),card.getScore()));
    }
    
    @Override
    public void houseBustCard(PlayingCard card, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, "Intermediate data to log .. String.format() is good here!");
-      // TODO: complete this method to log results
+      logger.log(Level.FINE, String.format("Card Dealt to House .. Suit: %s, Value: %s, Score: %d ... YOU BUSTED!",card.getSuit(),card.getValue(),card.getScore()));
    }
    
    @Override
    public void houseResult(int result, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, "Intermediate data to log .. String.format() is good here!");
-      // TODO: complete this method to log results
+      logger.log(Level.FINE, String.format("House, final result=%d",result));
    }
    
    @Override
    public void nextHouseCard(PlayingCard card, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, "Intermediate data to log .. String.format() is good here!");
-      // TODO: complete this method to log results
+      logger.log(Level.FINE, String.format("Card Dealt to House .. Suit: %s, Value: %s, Score: %d",card.getSuit(),card.getValue(),card.getScore()));
    }
    
 }

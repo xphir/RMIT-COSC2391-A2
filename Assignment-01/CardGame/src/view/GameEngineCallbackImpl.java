57efe0a7a40fefcf42c2b2b481cf28ab65d4e078
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-24 23:50:38 +1000

    temp fix for log.FINE

--- Assignment-01/CardGame/src/view/GameEngineCallbackImpl.java
+++ Assignment-01/CardGame/src/view/GameEngineCallbackImpl.java
@@ -60,42 +60,47 @@ public class GameEngineCallbackImpl implements GameEngineCallback
    public void nextCard(Player player, PlayingCard card, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, String.format("Card Dealt to %s .. Suit: %s, Value: %s, Score: %d",player.getPlayerName(),card.getSuit(),card.getValue(),card.getScore()));
+	  //TODO SHOULD BE FINE BUT IT DOESNT WORK
+      logger.log(Level.INFO, String.format("Card Dealt to %s .. Suit: %s, Value: %s, Score: %d",player.getPlayerName(),card.getSuit(),card.getValue(),card.getScore()));
    }
 
    @Override
    public void result(Player player, int result, GameEngine engine)
    {
       // final results logged at Level.INFO
-      logger.log(Level.INFO, String.format("The Loser, final result=%d", result));
+      logger.log(Level.INFO, String.format("%s, final result=%d", player, result));
    }
 
    @Override
    public void bustCard(Player player, PlayingCard card, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, String.format("Card Dealt to %s .. Suit: %s, Value: %s, Score: %d ... YOU BUSTED!",player.getPlayerName(),card.getSuit(),card.getValue(),card.getScore()));
+	 //TODO SHOULD BE FINE BUT IT DOESNT WORK
+      logger.log(Level.INFO, String.format("Card Dealt to %s .. Suit: %s, Value: %s, Score: %d ... YOU BUSTED!",player.getPlayerName(),card.getSuit(),card.getValue(),card.getScore()));
    }
    
    @Override
    public void houseBustCard(PlayingCard card, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, String.format("Card Dealt to House .. Suit: %s, Value: %s, Score: %d ... YOU BUSTED!",card.getSuit(),card.getValue(),card.getScore()));
+	 //TODO SHOULD BE FINE BUT IT DOESNT WORK
+      logger.log(Level.INFO, String.format("Card Dealt to House .. Suit: %s, Value: %s, Score: %d ... YOU BUSTED!",card.getSuit(),card.getValue(),card.getScore()));
    }
    
    @Override
    public void houseResult(int result, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, String.format("House, final result=%d",result));
+	 //TODO SHOULD BE FINE BUT IT DOESNT WORK
+      logger.log(Level.INFO, String.format("House, final result=%d",result));
    }
    
    @Override
    public void nextHouseCard(PlayingCard card, GameEngine engine)
    {
       // intermediate results logged at Level.FINE
-      logger.log(Level.FINE, String.format("Card Dealt to House .. Suit: %s, Value: %s, Score: %d",card.getSuit(),card.getValue(),card.getScore()));
+	 //TODO SHOULD BE FINE BUT IT DOESNT WORK
+      logger.log(Level.INFO, String.format("Card Dealt to House .. Suit: %s, Value: %s, Score: %d",card.getSuit(),card.getValue(),card.getScore()));
    }
    
 }

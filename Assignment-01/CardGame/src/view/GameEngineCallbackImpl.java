8f428917316511926d4a77634db19fefb933b86d
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-27 01:33:06 +1000

    revert to orig

--- Assignment-01/CardGame/src/view/GameEngineCallbackImpl.java
+++ Assignment-01/CardGame/src/view/GameEngineCallbackImpl.java
@@ -15,36 +15,6 @@ import view.interfaces.GameEngineCallback;
  * @author Caspar Ryan
  * @see view.interfaces.GameEngineCallback
  * 
- * &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&& SPECIFICATION FROM PDF &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
- * 
- * The sole purpose of this class is to support the user interface (view) which in assignment part 1
- * consists of simple console/logging output. Therefore all this class needs to do is log data to the
- * console from the parameters passed to its methods. Apart from implementing the logging (we
- * recommend String.format() here) the main thing is to make sure you call the right method at the 
- * right time! (see below). You should also as much as possible make use the of the overidden
- * toString() methods you will implement in SimplePlayer and PlayingCardImpl since this will
- * simplify the logging!
- * 
- * The only class that will call the GameEngineCallbackImpl methods is the GameEngineImpl 
- * class. For example as the dealPlayer(…) method is executing in a loop it will call the
- * nextCard(…) method on the GameEngineCallbackImpl (via the GameEngineCallback
- * interface). Details of the exact flow and where GameEngineCallback methods should be called are
- * provided in the GameEngineImpl source code and associated Javadoc. 
- * 
- * IMPORTANT: The main thing to watch out for (i.e. “gotcha”) is that this class should not manage any
- * game state or implement any game based functionality which instead belongs in the
- * GameEngineImpl. The core test here is that we should be able to replace your 
- * GameEngineCallbackImpl with our own (which obviously knows nothing about your
- * implementation) and your GameEngineImpl code should still work. This is a true test of 
- * encapsulation and programming using interfaces (i.e. to a specification) and is one of the main
- * objectives of this assignment! 
- * 
- * IF YOU DO NOT FOLLOW THE NOTE ABOVE YOUR CODE WILL NOT EXECUTE PROPERLY
- * WITH OUR TEST HARNESS AND YOU WILL LOSE MARKS! PLEASE DON’T GET CAUGHT OUT ..
- * IF IN DOUBT ASK, WE ARE HAPPY TO HELP :)
- * 
- * &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
- * 
  */
 public class GameEngineCallbackImpl implements GameEngineCallback
 {

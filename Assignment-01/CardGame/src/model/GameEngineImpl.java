e38ddea370439e4e93ac5743694abfacfbda9f8e
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-27 01:25:45 +1000

    added betHandle()

--- Assignment-01/CardGame/src/model/GameEngineImpl.java
+++ Assignment-01/CardGame/src/model/GameEngineImpl.java
@@ -121,12 +121,14 @@ public class GameEngineImpl implements GameEngine {
 		}
 		return false;
 	}
-
+	
+	//DONE
 	@Override
 	public void addGameEngineCallback(GameEngineCallback gameEngineCallback) {
 		gameEngineCallbacks.add(gameEngineCallback);
 	}
 
+	//DONE
 	@Override
 	public boolean removeGameEngineCallback(GameEngineCallback gameEngineCallback) {
 		for (GameEngineCallback aGameEngineCallback : gameEngineCallbacks) {
@@ -136,13 +138,15 @@ public class GameEngineImpl implements GameEngine {
 		}
 		return false;
 	}
-
+	
+	//DONE
 	@Override
 	public Collection<Player> getAllPlayers() {
 		List<Player> immutablePlayerLists = Collections.unmodifiableList(players);
 		return immutablePlayerLists;
 	}
-
+	
+	//DONE
 	@Override
 	public boolean placeBet(Player player, int bet) {
 		boolean result;
@@ -150,6 +154,7 @@ public class GameEngineImpl implements GameEngine {
 		return result;
 	}
 
+	//DONE - Might be better alway keeping it as a deque and shuffling that
 	@Override
 	public Deque<PlayingCard> getShuffledDeck() {
 		List<PlayingCard> newDeck = new ArrayList<PlayingCard>();
@@ -161,7 +166,7 @@ public class GameEngineImpl implements GameEngine {
 				newDeck.add(new PlayingCardImpl(suit, value));
 			}
 		}
-		// Shuffles the new deck
+		// Shuffles the new deck - cant shuffle a deque
 		Collections.shuffle(newDeck);
 
 		// converts the deck into Deque form
@@ -172,6 +177,7 @@ public class GameEngineImpl implements GameEngine {
 		return shuffledDeck;
 	}
 
+	//used to wait the delay inputed
 	private void wait(int delay) {
 		try {
 			Thread.sleep(delay);
@@ -180,6 +186,7 @@ public class GameEngineImpl implements GameEngine {
 		}
 	}
 
+	//THIS HANDLES HOSTS
 	//called by dealHost, does the logging and score management of dealHost, and on busting will return false
 	private boolean bustHandle(int handScore, PlayingCard card) {
 		int finalResult;
@@ -188,6 +195,7 @@ public class GameEngineImpl implements GameEngine {
 				aGameEngineCallback.houseBustCard(card, this);
 			}
 			finalResult = handScore - card.getScore();
+			betHandle(finalResult);
 			for (GameEngineCallback aGameEngineCallback : gameEngineCallbacks) {
 				aGameEngineCallback.houseResult(finalResult, this);
 			}
@@ -201,6 +209,7 @@ public class GameEngineImpl implements GameEngine {
 
 	}
 	
+	//THIS HANDLES PLAYERS
 	//Method overloading the host version of bustHandle, see above
 	private boolean bustHandle(Player player, int handScore, PlayingCard card) {
 		int finalResult;
@@ -222,6 +231,24 @@ public class GameEngineImpl implements GameEngine {
 			}
 			return true;
 		}
+	}
+	
+	//Handles the end of round bet returns and the reset of bets.
+	private void betHandle(int houseScore) {
+		for (Player aplayer : players) {
+			if (aplayer.getResult() >  houseScore) {
+				//player wins
+				aplayer.setPoints(aplayer.getPoints() + aplayer.getBet());
+				aplayer.resetBet();
+			}
+			else if (aplayer.getResult() ==  houseScore){
+				aplayer.resetBet();
+			} else {
+				aplayer.setPoints(aplayer.getPoints() - aplayer.getBet());;
+				aplayer.resetBet();
+			}
+		}
+
 	}
 
 }

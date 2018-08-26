677feaca8776d9023ccdec6b0d3c60b4b47846f9
Elliot Schot <s3530160@student.rmit.edu.au>
2018-08-27 03:22:51 +1000

    added x player and x hands tests
    completed 50+ players and 50+ hands fine

--- Assignment-01/CardGame/src/client/MyTestClient.java
+++ Assignment-01/CardGame/src/client/MyTestClient.java
@@ -1,6 +1,8 @@
 package client;
 
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,53 +29,44 @@ import view.GameEngineCallbackImpl;
  * @author Caspar Ryan
  * 
  */
-public class MyTestClient
-{
+public class MyTestClient {
 	private static Logger logger = Logger.getLogger("assignment1");
 
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		final GameEngine gameEngine = new GameEngineImpl();
-
-		// call method in Validator.jar to test *structural* correctness
-		// just passing this does not mean it actually works .. you need to test
-		// yourself!
-		// pass false if you want to disable logging .. (i.e. once it passes)
+		List<Player> testPlayers = new ArrayList<Player>();
+		int playerCount = 30;
+		int reqRunCount = 10;
+		int runCount = 0;
+		
 		Validator.validate(true);
-
-		// create two test players
-
-		Player[] players =
-				new Player[] { new SimplePlayer("1", "The Shark", 1000), new SimplePlayer("2", "The Loser", 500) };
+		
+		// create test players
+		for (int i = 1; i <= playerCount; i++) {
+			testPlayers.add(new SimplePlayer(Integer.toString(i), "Player #" + Integer.toString(i), 100));
+		}
 
 		// add logging callback
 		gameEngine.addGameEngineCallback(new GameEngineCallbackImpl());
 
 		// main loop to add players, place a bet and receive hand
-		for (Player player : players)
-		{
+		for (Player player : testPlayers) {
 			gameEngine.addPlayer(player);
-			gameEngine.placeBet(player, 100);
-			gameEngine.dealPlayer(player, 1);
 		}
 
-		gameEngine.dealHouse(10);
-
-		do
-		{
-			for (Player player : players)
-			{
+		do {
+			for (Player player : testPlayers) {
 				gameEngine.placeBet(player, 100);
 				gameEngine.dealPlayer(player, 1);
 			}
 
 			gameEngine.dealHouse(10);
-		} while (true);
+			runCount++;
+		} while (runCount < reqRunCount);
 	}
 
 	@SuppressWarnings("unused")
-	private static void printCards(Deque<PlayingCard> deck)
-	{
+	private static void printCards(Deque<PlayingCard> deck) {
 		for (PlayingCard card : deck)
 			logger.log(Level.INFO, card.toString());
 	}
